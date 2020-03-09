65ff1c79b89131dc4715738f5f2cafaa50cf9541
WebMoksed <mmislam1994@gmail.com>
2020-03-09 11:25:58 +0600

    done test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,66 @@
-$("#myTabs a").click(function(e) {
-  e.preventDefault();
+$(document).on("click", "#myTabs li", function() {
+  var tab_div_id = $(this)
+    .find("a")
+    .attr("href");
 
-  var url = $(this).attr("data-url");
-  var href = this.hash;
-  var pane = $(this);
+  $("#myTabs li").removeClass("active");
 
-  // ajax load from data-url
-  $(href).load(url, function(result) {
-    pane.tab("show");
-  });
+  $(this).addClass("active");
+
+  var data_count = $(tab_div_id).html().length;
+
+  var loader = '<div class="tab-content-loader">Loading pleaseee</div>';
+
+  $("#tab-content-id-pane")
+    .find(".tab-content-loader")
+    .remove();
+
+  if (data_count < 50) {
+    $("#tab-content-id-pane .tab-pane").hide();
+    $("#tab-content-id-pane").append(loader);
+
+    var url = "https://jsonplaceholder.typicode.com/posts";
+
+    $.ajax({
+      type: "GET",
+      crossOrigin: true,
+      crossDomain: true,
+      url: url,
+      dataType: "json",
+      async: false,
+      headers: {
+        accept: "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      success: function(data) {
+        //  console.log(data);
+
+        var body_data = "";
+        data.map((val, key) => {
+          body_data += createTr(val);
+        });
+
+        doneMap(tab_div_id, body_data);
+      }
+    });
+  }
 });
 
-// load first tab content
-$("#home").load($(".active a").attr("data-url"), function(result) {
-  $(".active a").tab("show");
-});
+function doneMap(tab_div_id, body_data) {
+  var table =
+    "<table class='table'> <thead><tr> <td>Title</td><td>Topics</td></tr></thead> <tbody>" +
+    body_data +
+    "</tobody></table>";
+
+  $(tab_div_id).html(table);
+  $("#tab-content-id-pane")
+    .find(".tab-content-loader")
+    .remove();
+  $(tab_div_id).show();
+}
+
+function createTr(data) {
+  var row = "<tr> <td>" + data.title + " </td><td>" + data.body + "</td> </tr>";
+
+  return row;
+}
